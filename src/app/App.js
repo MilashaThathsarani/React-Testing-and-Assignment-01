d9b8711806f94717d5fff9e0183045014bcc703b
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-07-21 10:55:22 +0530

    Updated

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import HomePage from '../../src/pages/home/index'
+
 function App() {
   return (
-    <div className="App">
+      <HomePage/>
 
-    </div>
   );
 }
 
-export default App;
+export default HomePage;
