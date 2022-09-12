752536f1dd3aafc8322430d91bffc042f34db53c
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-09-12 23:07:40 +0530

    Updated Login

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Customer from "../pages/Customer";
 import Item from "../pages/Item";
+import Login from "../pages/Login";
 
 function App() {
   return (
       //<HomePage name ="GDSE" status= "Busy"/>
      // <DashBoard/>
      //  <Customer/>
-      <Item/>
+      //<Item/>
+      <Login/>
   );
 }
 
