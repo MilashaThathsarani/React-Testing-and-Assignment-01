1c8391ef25548b5871968f1b47f7e3dbf4e076b5
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-09-13 11:07:22 +0530

    Updated

--- src/app/App.js
+++ src/app/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import Customer from "../pages/Customer";
 import Item from "../pages/Item";
 import Login from "../pages/Login";
+import HomePage from "../pages/home";
 
 function App() {
   return (
-      //<HomePage name ="GDSE" status= "Busy"/>
+      <HomePage name ="GDSE" status= "Busy"/>
      // <DashBoard/>
      //  <Customer/>
       //<Item/>
-      <Login/>
+      //<Login/>
   );
 }
 
